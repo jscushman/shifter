65bea2a6e47b9acd16a496ad6ff445bf3118e171
Jeremy Cushman <jscushman@gmail.com>
2016-03-25 10:42:48 -0400

    Remove json

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,13 +2,11 @@ class AppointmentsController < ApplicationController
   before_action :set_appointment, only: [:show, :edit, :update, :destroy]
 
   # GET /appointments
-  # GET /appointments.json
   def index
     @appointments = Appointment.all
   end
 
   # GET /appointments/1
-  # GET /appointments/1.json
   def show
   end
 
@@ -22,43 +20,28 @@ class AppointmentsController < ApplicationController
   end
 
   # POST /appointments
-  # POST /appointments.json
   def create
     @appointment = Appointment.new(appointment_params)
-
-    respond_to do |format|
-      if @appointment.save
-        format.html { redirect_to @appointment, notice: 'Appointment was successfully created.' }
-        format.json { render :show, status: :created, location: @appointment }
-      else
-        format.html { render :new }
-        format.json { render json: @appointment.errors, status: :unprocessable_entity }
-      end
+    if @appointment.save
+      redirect_to @appointment, notice: 'Appointment was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /appointments/1
-  # PATCH/PUT /appointments/1.json
   def update
-    respond_to do |format|
-      if @appointment.update(appointment_params)
-        format.html { redirect_to @appointment, notice: 'Appointment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @appointment }
-      else
-        format.html { render :edit }
-        format.json { render json: @appointment.errors, status: :unprocessable_entity }
-      end
+    if @appointment.update(appointment_params)
+      redirect_to @appointment, notice: 'Appointment was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /appointments/1
-  # DELETE /appointments/1.json
   def destroy
     @appointment.destroy
-    respond_to do |format|
-      format.html { redirect_to appointments_url, notice: 'Appointment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to appointments_url, notice: 'Appointment was successfully destroyed.'
   end
 
   private
--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,13 +2,11 @@ class CalendarsController < ApplicationController
   before_action :set_calendar, only: [:show, :edit, :update, :destroy]
 
   # GET /calendars
-  # GET /calendars.json
   def index
     @calendars = Calendar.all
   end
 
   # GET /calendars/1
-  # GET /calendars/1.json
   def show
   end
 
@@ -22,43 +20,28 @@ class CalendarsController < ApplicationController
   end
 
   # POST /calendars
-  # POST /calendars.json
   def create
     @calendar = Calendar.new(calendar_params)
-
-    respond_to do |format|
-      if @calendar.save
-        format.html { redirect_to @calendar, notice: 'Calendar was successfully created.' }
-        format.json { render :show, status: :created, location: @calendar }
-      else
-        format.html { render :new }
-        format.json { render json: @calendar.errors, status: :unprocessable_entity }
-      end
+    if @calendar.save
+      redirect_to @calendar, notice: 'Calendar was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /calendars/1
-  # PATCH/PUT /calendars/1.json
   def update
-    respond_to do |format|
-      if @calendar.update(calendar_params)
-        format.html { redirect_to @calendar, notice: 'Calendar was successfully updated.' }
-        format.json { render :show, status: :ok, location: @calendar }
-      else
-        format.html { render :edit }
-        format.json { render json: @calendar.errors, status: :unprocessable_entity }
-      end
+    if @calendar.update(calendar_params)
+      redirect_to @calendar, notice: 'Calendar was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /calendars/1
-  # DELETE /calendars/1.json
   def destroy
     @calendar.destroy
-    respond_to do |format|
-      format.html { redirect_to calendars_url, notice: 'Calendar was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to calendars_url, notice: 'Calendar was successfully destroyed.'
   end
 
   private
