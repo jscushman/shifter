1863f27590989e893b278f4f6717382182433837
Jeremy Cushman <jscushman@gmail.com>
2016-03-25 12:07:01 -0400

    Basic linking framework around site

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  resources :appointments
+
   resources :people
   resources :groups
   resources :calendars
-  get 'welcome/index'
+  resources :appointments
+  get 'overview/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'overview#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
