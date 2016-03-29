56b646239e6acdf9ca6d92ca4e84924c91f6dcd4
Jeremy Cushman <jscushman@gmail.com>
2016-03-29 18:06:21 -0400

    Allow new account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :authenticate_user
+  before_action :authenticate_user, only: [:index, :show, :edit, :update]
   before_action :authenticate_admin, only: [:index]
   
   def index
