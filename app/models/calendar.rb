73a36400ea761f67812cefdf89e8562bcb785b5d
Jeremy Cushman <jscushman@gmail.com>
2016-03-31 17:44:13 -0400

    Require no_credit_day field for calendar

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,7 +1,7 @@
 class Calendar < ActiveRecord::Base
   has_many :appointments
   
-  validates :title, :description, :max_simultaneous, :days_per_credit, :min_days, :start_end_day, presence: true
+  validates :title, :description, :max_simultaneous, :days_per_credit, :min_days, :start_end_day, :no_credit_day, presence: true
   
   validates_inclusion_of :max_simultaneous, in: 0..99
   validates_inclusion_of :days_per_credit, in: 0..99
