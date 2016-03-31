e9ad290d64909f986bca6c89c3547e95f86fd778
Jeremy Cushman <jscushman@gmail.com>
2016-03-31 18:47:32 -0400

    Test fix for rails relative path in sass

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,3 +37,20 @@ module ActionView
     end
   end
 end
+
+module Sass
+  module Rails
+    module Helpers
+      protected
+      def public_path(asset, kind)
+        resolver = options[:custom][:resolver]
+        asset_paths = resolver.context.asset_paths
+        path = resolver.public_path(asset, kind.pluralize)
+        if !asset_paths.send(:has_request?) && ENV['RAILS_RELATIVE_URL_ROOT']
+          path = ENV['RAILS_RELATIVE_URL_ROOT'] + path
+        end
+        path
+      end
+    end
+  end
+end
