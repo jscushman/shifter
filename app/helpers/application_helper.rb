8c6b1d6aa7d2e2088077dcec3b46541207d5ec52
Jeremy Cushman <jscushman@gmail.com>
2016-03-31 18:56:05 -0400

    Hard code font path to fix bug in sprockets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,20 +37,3 @@ module ActionView
     end
   end
 end
-
-module Sass
-  module Rails
-    module Helpers
-      protected
-      def public_path(asset, kind)
-        resolver = options[:custom][:resolver]
-        asset_paths = resolver.context.asset_paths
-        path = resolver.public_path(asset, kind.pluralize)
-        if !asset_paths.send(:has_request?) && ENV['RAILS_RELATIVE_URL_ROOT']
-          path = ENV['RAILS_RELATIVE_URL_ROOT'] + path
-        end
-        path
-      end
-    end
-  end
-end
