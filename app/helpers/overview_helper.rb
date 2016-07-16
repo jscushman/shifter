60580238ae2911086d6e0677878f540c3d0c2c1d
Jeremy Cushman <jscushman@gmail.com>
2016-07-16 12:17:30 -0400

    Show next available shifts on home page

--- app/helpers/overview_helper.rb
+++ app/helpers/overview_helper.rb
@@ -6,4 +6,25 @@ module OverviewHelper
   def total_shift_days appointment
     (appointment.ends - appointment.starts + 1).to_i
   end
+  
+  def next_empty_shift calendar
+    if calendar.appointments.on(Date.today).size == 0
+      return "Today"
+    elsif calendar.appointments.on(Date.tomorrow).size == 0
+      return "Tomorrow"
+    end
+    for day in (Date.today + 2)..(Date.today + 365)
+      if calendar.appointments.on(day).size == 0
+        return day.strftime("%A, %B %-d, %Y")
+      end
+    end
+    return "All covered for the next year"
+  end
+  
+  def output_shifts calendar, date
+    shifts = Array.new
+    calendar.appointments.on(date).each do |appointment|
+      shifts.push(appointment)
+    end
+  end
 end
