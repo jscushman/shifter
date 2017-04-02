02b86f6c28b8724af84462f864d078820ae97a13
Jeremy Cushman <jscushman@gmail.com>
2017-04-02 10:06:31 +0200

    Show correct shift credits for partially completed shifts

--- app/helpers/credits_helper.rb
+++ app/helpers/credits_helper.rb
@@ -1,12 +1,18 @@
 module CreditsHelper
-  def shift_credit(appointment, start_year, end_year)
+  def shift_credit(appointment, start_year, end_year, scheduled_only)
     starts = appointment.starts
-    if (starts.year < start_year.to_i)
+    ends = appointment.ends
+    if scheduled_only and starts <= Date.today
+      starts = Date.today + 1
+    end
+    if starts.year < start_year.to_i
       starts = (start_year + "-01-01").to_datetime
     end
-    ends = appointment.ends
-    if (ends.year > end_year.to_i)
+    if ends.year > end_year.to_i
       ends = (end_year + "-12-31").to_datetime
+    end
+    if not scheduled_only and ends > Date.today
+      ends = Date.today
     end
     days = (ends - starts + 1).to_i
     if appointment.calendar.no_credit_day
@@ -18,7 +24,7 @@ module CreditsHelper
   def sum_credits(group, start_year, end_year)
     total_credits = 0
     group.appointments.inyears(start_year, end_year).started.incals(@cals_to_show).forcredit.each do |shift|
-      total_credits += shift_credit shift, start_year, end_year
+      total_credits += shift_credit shift, start_year, end_year, false
     end
     return total_credits.round(2)
   end
@@ -26,7 +32,7 @@ module CreditsHelper
   def scheduled_credits(group, start_year, end_year)
     total_credits = 0
     group.appointments.inyears(start_year, end_year).scheduled.incals(@cals_to_show).forcredit.each do |shift|
-      total_credits += shift_credit shift, start_year, end_year
+      total_credits += shift_credit shift, start_year, end_year, true
     end
     return total_credits.round(2)
   end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,7 +7,7 @@ class Appointment < ActiveRecord::Base
   scope :startson, -> (date) { where("starts == ?", date) }
   scope :after, -> (date) { where("ends >= ?", date) }
   scope :started, -> { where("starts <= ?", Time.now.to_formatted_s(:db)) }
-  scope :scheduled, -> { where("starts > ?", Time.now.to_formatted_s(:db)) }
+  scope :scheduled, -> { where("ends > ? ", Time.now.to_formatted_s(:db)) }
   scope :inyears, -> (start_year, end_year) { where("strftime('%Y', starts) <= ? AND strftime('%Y', ends) >= ?", end_year, start_year) }
   scope :incals, -> (cal_list) { where(calendar_id: cal_list.map(&:id)) }
   scope :forcredit, -> { where("credit == ?", true) }
