b7dc93a77d8f53a65b79bca30276c5a5e90a4df1
Jeremy Cushman <jscushman@gmail.com>
2017-04-19 19:12:53 -0400

    Delete appointments if shifter is deleted

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
-  has_many :appointments
+  has_many :appointments, dependent: :destroy
   has_many :credited_appointments, -> { forcredit }, class_name: "Appointment"
 
   default_scope { order('name ASC') }
